c53b871f3cbb7f6da41554484a52c77605da089a
doVanNhat <dovannhatdn94@gmail.com>
2023-12-27 18:54:38 +0700

    add coverage

--- jest.config.js
+++ jest.config.js
@@ -11,4 +11,12 @@ module.exports = {
   },
   verbose: true,
   setupFilesAfterEnv: ['./src/jest.setup.ts'],
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: -10,
+    },
+  },
 };
